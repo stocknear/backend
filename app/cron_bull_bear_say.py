a50ae8e5f3da88c390d32e17c242af37691329c7
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-12-06 22:47:02 +0100

    update bulls bear say

--- app/cron_bull_bear_say.py
+++ app/cron_bull_bear_say.py
@@ -15,31 +15,32 @@ api_key = os.getenv('BENZINGA_API_KEY')
 async def get_endpoint(session, symbol):
     url = "https://api.benzinga.com/api/v1/bulls_bears_say"
     querystring = {"token": api_key, "symbols": symbol}
-    
+    formatted_data = {}
     try:
         async with session.get(url, params=querystring) as response:
             res = ujson.loads(await response.text())
             try:
                 for item in res['bulls_say_bears_say']:
                     date = datetime.fromtimestamp(item['updated'])
-                    date = date.strftime("%B %d, %Y")
+                    date = date.strftime("%Y-%m-%d %H:%M:%S")
                     bull_case = item['bull_case']
                     bear_case = item['bear_case']
                     formatted_data = {'date': date, 'bullSays': bull_case, 'bearSays': bear_case}
-            except Exception as e:
-                formatted_data = {}
-    except Exception as e:
-        formatted_data = {}
-        print(e)
-    with open(f"json/bull_bear_say/{symbol}.json", 'w') as file:
-        ujson.dump(formatted_data, file)
+            except:
+                pass
+    except:
+        pass
+
+    if formatted_data:
+        with open(f"json/bull_bear_say/{symbol}.json", 'w') as file:
+            ujson.dump(formatted_data, file)
 
 async def run():
     con = sqlite3.connect('stocks.db')
 
     cursor = con.cursor()
     cursor.execute("PRAGMA journal_mode = wal")
-    cursor.execute("SELECT DISTINCT symbol FROM stocks")
+    cursor.execute("SELECT DISTINCT symbol FROM stocks WHERE symbol NOT LIKE '%.%'")
     stocks_symbols = [row[0] for row in cursor.fetchall()]
     #stocks_symbols = ['NVDA']
     con.close()
