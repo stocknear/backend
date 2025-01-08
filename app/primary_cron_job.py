58d0791dc742cac7159580294e2d3e357c911818
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-01-08 16:19:37 +0100

    bugfixing

--- app/primary_cron_job.py
+++ app/primary_cron_job.py
@@ -345,7 +345,7 @@ def run_threaded(job_func):
 # Schedule the job to run
 
 schedule.every().day.at("00:00").do(run_threaded, run_options_jobs).tag('options_job')
-schedule.every().day.at("01:00").do(run_threaded, run_db_schedule_job)
+schedule.every().day.at("02:00").do(run_threaded, run_db_schedule_job)
 #schedule.every().day.at("05:00").do(run_threaded, run_options_gex).tag('options_gex_job')
 schedule.every().day.at("05:00").do(run_threaded, run_export_price).tag('export_price_job')
 
