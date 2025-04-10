691d062d8586903e9baef511add8e10211f738f5
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-04-10 11:34:14 +0200

    extend info text

--- app/cron_info_text.py
+++ app/cron_info_text.py
@@ -679,7 +679,57 @@ data = {
     "otherExpenses": {
         "text": "Other Expenses encompass costs that fall outside the primary operational activities of the company. This category includes non-recurring items such as restructuring charges, asset impairments, legal settlements, and losses from discontinued operations. These expenses are reported separately to provide a clearer picture of the company's core operational performance."
     },
-
+    "netCashProvidedByOperatingActivities": {
+        "text": "Operating cash flow, also called cash flow from operating activities, measures the amount of cash that a company generates from normal business activities. It is the amount of cash left after all cash income has been received, and all cash expenses have been paid."
+    },
+    "capitalExpenditure": {
+        "text": "Capital expenditures are also called payments for property, plants and equipment. It measures cash spent on long-term assets that will be used to run the business, such as manufacturing equipment, real estate and others."
+    },
+    "acquisitionsNet": {
+        "text": "Cash paid for acquisitions, such as the acquisition of a company, a stake in a subsidiary, or a new product line."
+    },
+    "purchasesOfInvestments": {
+        "text": "Purchase of Investments refers to the acquisition of financial assets like stocks or bonds by a company, typically for investment purposes. It represents an outflow of cash used to buy these investments."
+    },
+    "salesMaturitiesOfInvestments": {
+        "text": "Sales Maturities of Investments signifies the selling or maturity of financial assets like stocks or bonds by a company. It represents an inflow of cash resulting from these investment activities."
+    },
+    "netDebtIssuance": {
+        "text": "Debt Repayment is the process of paying off loans or debt obligations. It represents an outflow of cash as the company reduces its outstanding debt."
+    },
+    "netDividendsPaid": {
+        "text": "The total amount paid out as cash dividends to shareholders."
+    },
+    "otherFinancingActivities": {
+        "text": "Other financial activities includes miscellaneous financial transactions beyond regular operations that impact a company's cash flow."
+    },
+    "commonStockRepurchased": {
+        "text": "The cash gained from issuing shares, or cash spent on repurchasing shares via share buybacks. A positive number implies that the company issued more shares than it repurchased. A negative number implies that the company bought back shares."
+    },
+    "netCashProvidedByInvestingActivities": {
+        "text": "Investing cash flow is the total change in cash from buying and selling investments and long-term assets."
+    },
+    "deferredIncomeTax": {
+        "text": "Deferred income tax refers to future tax liabilities or assets resulting from differences in how a company reports income for tax purposes versus financial reporting. It represents the amount of income tax that will be paid or saved in the future due to these differences."
+    },
+    "otherWorkingCapital": {
+        "text": "Other working capital represents miscellaneous changes in short-term assets and liabilities that don't have specific categories, affecting the company's available cash."
+    },
+    "otherNonCashItems": {
+        "text": "Other Non-Cash Items refers to non-cash transactions or adjustments that impact the company's financial performance but don't involve actual cash flows. These can include items like depreciation, amortization, or changes in the fair value of investments."
+    },
+    "depreciationAndAmortization": {
+        "text": "Depreciation and amortization are accounting methods for calculating how the value of a business's assets change over time. Depreciation refers to physical assets, while amortization refers to intangible assets."
+    },
+    "netChangeInCash": {
+        "text": "Net cash flow is the sum of the operating, investing and financing cash flow numbers. It is the change in cash and equivalents on the company's balance sheet during the accounting period. It is often shown as increase/decrease in cash and equivalents on the cash flow statement."
+    },
+    "otherInvestingActivities": {
+        "text": "Other investing activities are investing activities that do not belong to any of the categories above. "
+    },
+    "changeInWorkingCapital": {
+        "text": "Change in working capital is the difference between a company's short-term assets and liabilities over a specific period, reflecting how much cash flow is impacted by these changes."
+    },
 
 }
 
