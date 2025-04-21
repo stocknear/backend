19d22cea3714df2360862b5f49e6d72288f7f66c
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-04-21 23:54:13 +0200

    update index db

--- app/create_index_db.py
+++ app/create_index_db.py
@@ -220,7 +220,9 @@ db = IndexDatabase('backup_db/index.db')
 loop = asyncio.get_event_loop()
 all_tickers = loop.run_until_complete(fetch_tickers())
 
-all_tickers = [item for item in all_tickers if item['symbol'] in ['^SPX','^VIX']]
+all_tickers = [item for item in all_tickers if item['currency'] == 'USD']
+print(len(all_tickers))
+
 try:
     loop.run_until_complete(db.save_index(all_tickers))
 except Exception as e:
