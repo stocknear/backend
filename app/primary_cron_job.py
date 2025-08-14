07b38970fb92af5f45962e0709fabc4ae74e10c7
MuslemRahimi <mrahimi@stocknear.com>
2025-08-14 16:53:26 +0200

    update cron job

--- app/primary_cron_job.py
+++ app/primary_cron_job.py
@@ -440,7 +440,7 @@ schedule.every().saturday.at("05:00").do(run_threaded, run_ownership_stats).tag(
 
 schedule.every(30).minutes.do(run_threaded, run_dividend_list).tag('dividend_list_job')
 schedule.every(3).hours.do(run_threaded, run_congress_trading).tag('congress_job')
-schedule.every(20).minutes.do(run_threaded, run_cron_market_news).tag('market_news_job')
+schedule.every(30).minutes.do(run_threaded, run_cron_market_news).tag('market_news_job')
 
 schedule.every(30).minutes.do(run_threaded, run_cron_industry).tag('industry_job')
 
@@ -471,7 +471,7 @@ schedule.every(15).minutes.do(run_threaded, run_dark_pool_level).tag('dark_pool_
 schedule.every(10).minutes.do(run_threaded, run_dark_pool_flow).tag('dark_pool_flow_job')
 
 schedule.every(2).minutes.do(run_threaded, run_dashboard).tag('dashboard_job')
-schedule.every(2).minutes.do(run_threaded, run_quote).tag('quote_job')
+schedule.every(3).minutes.do(run_threaded, run_quote).tag('quote_job')
 
 
 schedule.every(15).seconds.do(run_threaded, run_if_not_running(run_cron_options_flow, 'options_flow_job')).tag('options_flow_job')
