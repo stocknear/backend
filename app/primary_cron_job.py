1067804815bd2683c5d0d96232af72df62e226b0
MuslemRahimi <mrahimi@stocknear.com>
2025-10-03 14:41:22 +0200

    reduce timer

--- app/primary_cron_job.py
+++ app/primary_cron_job.py
@@ -452,7 +452,7 @@ schedule.every(8).minutes.do(run_threaded, run_one_day_price).tag('one_day_price
 schedule.every(20).minutes.do(run_threaded, run_tracker).tag('tracker_job')
 
 
-schedule.every(10).minutes.do(run_threaded, run_market_moods).tag('market_moods_job')
+schedule.every(5).minutes.do(run_threaded, run_market_moods).tag('market_moods_job')
 schedule.every(10).minutes.do(run_threaded, run_earnings).tag('earnings_job')
 
 
